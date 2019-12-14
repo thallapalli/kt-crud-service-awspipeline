1d4a0ccdf3aa55ded3059a4b020ec32dbb8d07d2
karnakar <thallapallikarnakar@gmail.com>
2019-12-14 12:17:28 -0500

    hello from branch

--- src/main/java/com/demo/crud/crudservice/controller/CrudController.java
+++ src/main/java/com/demo/crud/crudservice/controller/CrudController.java
@@ -20,7 +20,7 @@ public class CrudController {
 
 	@GetMapping(value = "/greetme/{name}")
 	public String greetme(@PathVariable String name) {
-		return "Hello " + name;
+		return "Hello from branch " + name;
 	}
 
 	@GetMapping(value = "/retrievenotes")
