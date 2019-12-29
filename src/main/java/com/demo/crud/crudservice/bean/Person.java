023f57444e5a08598533868cc452c946892f2889
karnakar <karnakar@ymail.com>
2019-12-29 17:14:09 -0500

    Person added

---
+++ src/main/java/com/demo/crud/crudservice/bean/Person.java
@@ -0,0 +1,13 @@
+package com.demo.crud.crudservice.bean;
+
+public class Person {
+private String name;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+}
